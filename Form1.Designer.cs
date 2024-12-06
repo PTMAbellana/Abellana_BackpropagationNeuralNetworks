ae7814471a9fe5604e85df6e9f575f6e2030c746
PTMAbellana <147637660+PTMAbellana@users.noreply.github.com>
2024-12-06 20:42:49 +0800

    BNN Act Done

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Backpropagation_Neural_Networks
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            textBox9 = new TextBox();
+            label6 = new Label();
+            textBox10 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 206);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(114, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(73, 245);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(114, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(73, 283);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(114, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(73, 322);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(114, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(96, 169);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 20);
+            label1.TabIndex = 4;
+            label1.Text = "INPUTS";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(323, 245);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 20);
+            label3.TabIndex = 6;
+            label3.Text = "REAL VALUE";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(310, 269);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(114, 27);
+            textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(535, 269);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(114, 27);
+            textBox6.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(540, 245);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 20);
+            label4.TabIndex = 9;
+            label4.Text = "ROUNDED OFF";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Info;
+            button1.Location = new Point(769, 145);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 69);
+            button1.TabIndex = 10;
+            button1.Text = "INITIALIZE NEURAL NETWORK";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ScrollBar;
+            button2.Location = new Point(769, 222);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 69);
+            button2.TabIndex = 11;
+            button2.Text = "TRAIN";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.InactiveCaption;
+            button3.Location = new Point(769, 301);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(131, 69);
+            button3.TabIndex = 12;
+            button3.Text = "TEST";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.GradientInactiveCaption;
+            button4.Location = new Point(310, 426);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(124, 93);
+            button4.TabIndex = 13;
+            button4.Text = "FIND MINIMUM HIDDEN NEURONS";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.GradientActiveCaption;
+            button5.Location = new Point(540, 426);
+            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(114, 93);
+            button5.TabIndex = 16;
+            button5.Text = "FIND MINIMUM EPOCHS";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(480, 99);
+            textBox9.Margin = new Padding(3, 4, 3, 4);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(209, 27);
+            textBox9.TabIndex = 20;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(480, 75);
+            label6.Name = "label6";
+            label6.Size = new Size(209, 20);
+            label6.TabIndex = 19;
+            label6.Text = "CURRENT HNEURONS COUNT";
+            label6.Click += label6_Click;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(310, 99);
+            textBox10.Margin = new Padding(3, 4, 3, 4);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(124, 27);
+            textBox10.TabIndex = 22;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(310, 75);
+            label7.Name = "label7";
+            label7.Size = new Size(124, 20);
+            label7.TabIndex = 21;
+            label7.Text = "CURRENT EPOCH";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label8.ForeColor = Color.IndianRed;
+            label8.Location = new Point(73, 103);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 25);
+            label8.TabIndex = 23;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(310, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(378, 20);
+            label2.TabIndex = 24;
+            label2.Text = "BACKPROPAGATION NEURAL NETWORKS ACTIVITY";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(987, 600);
+            Controls.Add(label2);
+            Controls.Add(label8);
+            Controls.Add(textBox10);
+            Controls.Add(label7);
+            Controls.Add(textBox9);
+            Controls.Add(label6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private TextBox textBox9;
+        private Label label6;
+        private TextBox textBox10;
+        private Label label7;
+        private Label label8;
+        private Label label2;
+    }
+}
